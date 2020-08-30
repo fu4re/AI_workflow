a3aa2f6bc5a8a543bea7b92096c23c82c636dc6e
fu4re <errozergame@gmail.com>
2020-08-31 00:35:05 +0300

    Выгрузка датасета в базу данных

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from UCI import unzip_from_UCI
+from database_core import create_sqlite_db
 
 UCI_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00320/student.zip"
 unzip_from_UCI(UCI_url)
+
+create_sqlite_db(db="data\\students.sqlite", file_pattern="data\\*.csv")
