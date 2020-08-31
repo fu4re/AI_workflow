cf8ceaeaabf12b7f46d6c303a4247a123f24f114
fu4re <errozergame@gmail.com>
2020-08-31 13:59:50 +0300

    Реализовано объединение таблиц данных и их извлечение с помощью pandas

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from UCI import unzip_from_UCI
-from database_core import create_sqlite_db
+import os, sqlite3
+import pandas as pd
 
-UCI_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00320/student.zip"
-unzip_from_UCI(UCI_url)
+DIR_PATH = os.getcwd()
+DB_NAME = "data\\students.sqlite"
+CHUNK_SIZE = 2500
 
-create_sqlite_db(db="data\\students.sqlite", file_pattern="data\\*.csv")
+conn = sqlite3.connect(DB_NAME)
+conn.text_factory = str
+#  Объединяем таблицы с ежедневными и почасовыми данными
+sql = "SELECT H.*, D.cnt AS day_cnt FROM hour AS H INNER JOIN day as D ON (H.dteday = D.dteday)"
+DB_stream = pd.read_sql(sql, conn, chunksize=CHUNK_SIZE)
+
+for j, data_chunk in enumerate(DB_stream):
+    x, y = data_chunk.shape
+
+    print(f"Порция {j + 1} - Размер загруженной порции данных: {x} прецедентов, {y} признаков")
+    # Кор алгоритма
